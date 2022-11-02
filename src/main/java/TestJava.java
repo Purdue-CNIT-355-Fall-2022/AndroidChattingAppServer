ef8de397a31d97694799b1bd4312484efe7a9fdf
Doyoon Kim <doyoon3312@kakao.com>
2022-11-01 22:44:55 -0400

    [FEAT Initial Project Repository
      * Initial Project Repository for Android Chatting Application; Server-side
        - JDK: 15.0.1
        - Java/Kotlin

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,11 @@
+public class TestJava {
+
+    // Default constructor
+    public TestJava() {
+
+    }
+
+    public String getGreetingString(String name) {
+        return String.format("Hello, %s\n", name);
+    }
+}
---
+++ src/main/kotlin/TestKotlin.kt
@@ -0,0 +1,10 @@
+// create main function
+fun main(args: Array<String>) {
+
+    val name = "Doyoon"
+
+    // Get Greeting
+    val greetingMessage = TestJava().getGreetingString(name);
+
+    println(greetingMessage)
+}
